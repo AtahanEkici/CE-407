47c6cdd8308e8032b0115c32604d8a2ae5dcb54c
Ata <laythese@gmail.com>
2020-04-04 21:54:09 +0300

    Comments
    Comments added into various functions for improving understandiblity of the code  as well as the readibility

--- MysqlConnection.java
+++ MysqlConnection.java
@@ -5,6 +5,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+// As suggested from the 
 /**
  *
  * @author atahan ekici
@@ -14,15 +16,15 @@ public class MysqlConnection
 private static Connection con;
     public static void getConnection() throws InstantiationException, IllegalAccessException
 { 
-String url = "jdbc:mysql://localhost:3306/voter?useUnicode=true&useLegacyDatetimeCode=false&serverTimezone=Turkey"; 
-String username = "root";
-String password = "1234";
+String url = "jdbc:mysql://localhost:3306/voter?useUnicode=true&useLegacyDatetimeCode=false&serverTimezone=Turkey"; //modified to overcome server time zone issues //
+String username = "root"; // username for the database//
+String password = "1234"; // password for the database //
 
    try
 {
-Class.forName("com.mysql.cj.jdbc.Driver").newInstance();  
+Class.forName("com.mysql.cj.jdbc.Driver").newInstance();  // Using updated cj class file //
 
- con = DriverManager.getConnection(url,username,password);
+ con = DriverManager.getConnection(url,username,password); // 
 }catch(ClassNotFoundException | SQLException e)
 { 
    System.out.println(e);
@@ -35,20 +37,21 @@ Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
         ResultSet rs = s.executeQuery("select * from "+name+"");
         
 while(rs.next())
-        System.out.println(rs.getString(1)+"  "+rs.getInt(2)+"  "+rs.getString(3)+ " " +rs.getInt(4));
+        System.out.println(rs.getString(1)+"  "+rs.getInt(2)+"  "+rs.getString(3)+ " " +rs.getInt(4)); // geting data from 4 different columns //
     }
     
     public static void setData(String id, int code, String address, int age) throws SQLException
     {
         try{
-        PreparedStatement s = con.prepareStatement("insert into voter values(?,?,?,?)");
+        PreparedStatement s = con.prepareStatement("insert into voter values(?,?,?,?)"); // inserting corresponding tuples //
         
+        // PARAMATER LOADING
         s.setString(1,id);
         s.setInt(2,code);
         s.setString(3,address);
         s.setInt(4,age);
         
-        s.execute();
+        s.execute(); // executing the command //
        
     }catch(SQLException e)
     {
