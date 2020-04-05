084d932dd7fd8293912753cd1f0ea48f5e090225
Ata <laythese@gmail.com>
2020-04-05 12:02:47 +0300

    Bug fixes(2)
    Mistakes were made.

--- citizen_id control trigger.sql
+++ citizen_id control trigger.sql
@@ -7,10 +7,10 @@ CREATE TRIGGER ID_CONTROL BEFORE INSERT ON voter
    DECLARE numLength INT;
    SET numLength = (SELECT CHAR_LENGTH(NEW.citizen_id));
 
-   IF(numLength > 12) THEN
+   IF(numLength > 11) THEN
     signal sqlstate '45000' set message_text = 'Citizen ID requres 11 characters check your ID (too much characters)';
     
-    ELSEIF(numLength < 10) THEN
+    ELSEIF(numLength < 11) THEN
     signal sqlstate '45000' set message_text = 'Citizen ID requres 11 characters check your ID(too less characters)';
     
    END IF;
