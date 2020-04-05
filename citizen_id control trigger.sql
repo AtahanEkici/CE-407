cca587cde900521a53ce06b9868a9c37f9fd821d
Ata <laythese@gmail.com>
2020-04-05 11:43:33 +0300

    Addition
    Added new elseif statement for more cases of failure

--- citizen_id control trigger.sql
+++ citizen_id control trigger.sql
@@ -8,6 +8,10 @@ CREATE TRIGGER ID_CONTROL BEFORE INSERT ON voter
    SET numLength = (SELECT LENGTH(NEW.citizen_id));
 
    IF(numLength > 11) THEN
-    signal sqlstate '45000' set message_text = 'Citizen ID requres 11 characters check your ID';
+    signal sqlstate '45000' set message_text = 'Citizen ID requres 11 characters check your ID (too much characters)';
+    
+    ELSEIF(numLenght < 11) THEN
+    signal sqlstate '45000' set message_text = 'Citizen ID requres 11 characters check your ID(too less characters)';
+    
    END IF;
 END;
