8ce7f9db433c0ddd6c25e4887a4f9e655f0611ab
Ata <laythese@gmail.com>
2020-04-16 17:30:57 +0300

    New Functions
    Added 2 new functions

--- SendEmail.java
+++ SendEmail.java
@@ -3,7 +3,6 @@ package CE407;
 import java.util.ArrayList;
 import java.util.Properties;
 import java.util.Random;
-
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
@@ -11,18 +10,22 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import static CE407.MysqlConnection.updateCode;
+import java.sql.SQLException;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class SendEmail 
 {
 
- public static void SendMail(String receiver) {
+ public static void SendMail(String receiver) throws InstantiationException, IllegalAccessException, SQLException {
  // Add recipient
  String to = ""+receiver+"";
 
 // Add sender
  String from = "laythese@gmail.com";
- final String username = "laythese@gmail.com";// Gmail username //
- final String password = "YOURAUTHENTICATION PSW HERE";//Gmail password //
+ final String username = "laythese@gmail.com";//your Gmail username 
+ final String password = "dwzqtjtgmaxygsfe";//your Gmail password
 
 String host = "smtp.gmail.com";
 
@@ -53,7 +56,7 @@ try {
  message.setSubject("Your Code For Voting");
  
  String Code = CodeGenerator();
- 
+ updateCode(receiver,Code);
  
  // Put the content of your message
  message.setText("Your voting code is : "+Code+"");
@@ -63,7 +66,8 @@ try {
 
 System.out.println("Sent message successfully.... hopefully");
 
-} catch (MessagingException e) {
+} catch (MessagingException e) 
+{
  throw new RuntimeException(e);
  }
  }
@@ -77,11 +81,19 @@ System.out.println("Sent message successfully.... hopefully");
     {
         list.add(random.nextInt(10));
     }
-    return list.toString();
+    StringBuilder sb = new StringBuilder();
+    list.stream().map((Integer s) -> {
+        sb.append(s);
+        return s;
+    }).forEachOrdered((Integer _item) -> {
+         sb.append("");
+    });
+      String str = sb.toString();
+     return str;
 }  
+ 
+ public static void SendAll(String [] Mails)
+ {
+     //empty for now to be updated //
+ }
 }
-  
-
-
-  
-
