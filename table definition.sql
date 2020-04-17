688841f88d3b87a259c7f1bf68204c223f2ccf29
Ata <laythese@gmail.com>
2020-04-17 09:37:47 +0300

    Add files via upload

---
+++ table definition.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `voter`.`voter` (
+  `ID` VARCHAR(11) NOT NULL,
+  `Code` INT NULL,
+  `Tel_num` VARCHAR(45) NULL,
+  `Age` INT NULL,
+  `Address` VARCHAR(70) NULL,
+  `Email` VARCHAR(150) NOT NULL
+  PRIMARY KEY (`ID`));
