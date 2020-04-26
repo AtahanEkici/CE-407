d46e93ab31916672ce03ec245e9e36bb8f2536dd
Ata <laythese@gmail.com>
2020-04-26 21:50:20 +0300

    Add files via upload

---
+++ Tel-num_Control_Update.sql
@@ -0,0 +1,14 @@
+DELIMITER  //
+CREATE TRIGGER TEL_Control_UPDATE
+BEFORE UPDATE ON voter
+FOR EACH ROW
+
+BEGIN 
+
+DECLARE numLength INT;
+SET numLength = (SELECT CHAR_LENGTH(NEW.Tel_num));
+
+IF(numLength <> 10) THEN
+  signal sqlstate '45000' set message_text = 'Your telephone number should be 10 characters';
+END IF;
+END; 
