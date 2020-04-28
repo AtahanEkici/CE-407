3f832d49704dbea6656e88f02729b2f8819c3322
Ata <laythese@gmail.com>
2020-04-28 20:26:23 +0300

    New Trigger
    Trigger for checking the Password value for lenght.

---
+++ PASSWORD_CONTROL_UPDATE.sql
@@ -0,0 +1,14 @@
+DELIMITER //
+CREATE TRIGGER PASSWORD_CONTROL_UPDATE
+AFTER UPDATE ON voter
+FOR EACH ROW
+
+BEGIN
+
+DECLARE numLenght INT;
+SET numLenght = (SELECT char_length(NEW.Password));
+
+IF(numLneght < 8) THEN
+signal sqlstate '45000' set message_text = 'Your password is too short';
+END IF;
+END; 
