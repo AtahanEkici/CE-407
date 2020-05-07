feea98c83618c5f01fe4022286e3bcfc8225cafb
Ata <laythese@gmail.com>
2020-05-07 20:03:20 +0300

    Update citizen_id control trigger.sql

--- citizen_id control trigger.sql
+++ citizen_id control trigger.sql
@@ -1,11 +1,13 @@
 DELIMITER //
 
-CREATE TRIGGER ID_CONTROL BEFORE INSERT ON voter
- FOR EACH ROW 
- BEGIN
+CREATE TRIGGER ID_CONTROL
+BEFORE INSERT ON voter
+FOR EACH ROW
+
+BEGIN
 
    DECLARE numLength INT;
-   SET numLength = (SELECT CHAR_LENGTH(NEW.citizen_id));
+   SET numLength = (SELECT CHAR_LENGTH(NEW.ID));
 
    IF(numLength > 11) THEN
     signal sqlstate '45000' set message_text = 'Citizen ID requres 11 characters check your ID (too much characters)';
@@ -15,4 +17,4 @@ CREATE TRIGGER ID_CONTROL BEFORE INSERT ON voter
     
    END IF;
    
-END;
+END //
